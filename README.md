ea7a95e85c8e4f4513c8fa16ebe106e25bd6f63d
Roger Reyes <rreyes@cobiscorp.com>
2023-10-10 08:56:47 -0500

    Ajustes adicionales

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PingPongPython
+
+|Requerimiento|
+|---|
+|Se debe tener instalado python 3|
+
+## Ejecutar programa con Windows
+Para iniciar el juego en windows, se debe ejecutar el archivo 'init.bat'
--- init.bat
+++ init.bat
@@ -1,5 +1,10 @@
-python -m venv env
-env\Scripts\activate.bat
-python -m pip install -r requirements.txt
-python pingpong.py
-pause
+@echo off
+if exist "env\Scripts\Activate.bat" (
+    echo "La carpeta 'env' y el archivo 'Activate.bat' existen.";
+
+    env\Scripts\activate.bat && python pingpong.py 
+) else (
+    python -m venv env
+    echo La carpeta 'env' y/o el archivo 'Activate.bat' Creado.
+    env\Scripts\activate.bat && python -m pip install -r requirements.txt && python pingpong.py
+)
